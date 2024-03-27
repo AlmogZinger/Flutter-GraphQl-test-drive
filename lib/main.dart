c28d08d03f57fd2576e7472f6fbccb2e96087725
AlmogZinger <almog389@gmail.com>
2024-03-27 12:00:24 +0200

    Represent two repo and list of all repo of user

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,65 @@ class ScreenWidget extends StatelessWidget{
   }
 
 }
+class RepoDropBox extends StatefulWidget {
+  const RepoDropBox({super.key});
+
+  @override
+  State< RepoDropBox> createState() => RepoDropBoxState();
+}
+
+class RepoDropBoxState extends State<RepoDropBox> {
+  String repoDropBoxValue = "Peack a repo";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      width: 200.0,
+  height: 300.0,
+      child: Query(
+        options: 
+        QueryOptions(
+          document: gql(r'''
+         query listOfRepo($owner : String!){
+            user(login: $owner){
+                repositories(first: 100)
+                {
+                    nodes{
+                        name
+                    }
+                }
+            }
+        }
+        '''
+            ),
+            variables: const {
+              'owner' :"AlmogZinger" , 
+            }
+            ), 
+        builder: withGenericHandling((result, {fetchMore, refetch}){
+                  final list = result.data!['user']['repositories']['nodes'];
+                  List<dynamic> strList = list.map((value) => value['name'].toString()).toList();
+                    return  Expanded(
+                      child: ListView(
+                         children: [
+                        for (var repo in strList) 
+                        ListTile(
+                        leading: const Icon(Icons.favorite), 
+                        title: Text(repo),
+                        )
+                        ]
+                      ),
+                      );
+        }
+        )
+        ),
+    );
+  
+  }
+}
+
+
 
 class MyHomePage extends StatefulWidget{
   const MyHomePage ({
@@ -90,7 +149,15 @@ class MyHomePage extends StatefulWidget{
 }
 
 class MyHomePageState extends State<MyHomePage>{
-  String name_repo = "github_pr"; 
+  String name_repo = "Students-UI"; 
+  final myController = TextEditingController();
+
+    @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
 
   void changePr(String newRepo)
   {
@@ -98,6 +165,8 @@ class MyHomePageState extends State<MyHomePage>{
       name_repo = newRepo; 
     });
   }
+
+
     @override 
     Widget build (BuildContext context){
       return Scaffold(
@@ -106,13 +175,25 @@ class MyHomePageState extends State<MyHomePage>{
         ),
         body: Column(
             children: <Widget>[
-              Text("Name of the repo: $name_repo"),
+              Row( 
+                children: [
+                const Text("Name of the repo:"
+                ),
+                 TextButton(
+                  onPressed: (){ changePr(name_repo=="Students" ? "Students-UI" : "Students"); }, 
+                  child:  Text(name_repo)
+                 ),
+               const RepoDropBox()
+                 ]  
+             ),
+
+             
               Query(
                 options: QueryOptions(
                   document: gql( r'''
                               
 query allPullRequests($name_repo: String!) {
-  repository(owner: "nrbnlulu", name: $name_repo) {
+  repository(owner: "AlmogZinger", name: $name_repo) {
     pullRequests(first: 100) { 
       nodes {
         comments(first: 100) { 
@@ -142,7 +223,7 @@ query allPullRequests($name_repo: String!) {
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(20),
-                            child: Text ("in the repo $name_repo there is ${Comments.length} comments "),
+                            child: Text ("in the repo $name_repo there is ${Comments.length} pr "),
                             ),
                             for (var commentsG in Comments)  
                             for(var comments in commentsG["comments"]["nodes"])
